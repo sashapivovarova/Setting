7668dc6ada922c49784f711e8a1299618f98a693
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-19 18:50:45 +0900

    function と method の理解

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -7,39 +7,23 @@
 
 import SwiftUI
 
+func returnName() -> String {
+    return "sasha"
+} // function
+
 struct ContentView: View {
     
-    @State var won: Int = 1000
-    var yen: Int {
-        get {
-            return won/10
-        }
-    }
     var body: some View {
         VStack{
-            if won == 1000 {
-                Text("Reiwa")
-            } else {
-                Text("Syouwa")
-            }
-            Text("won is \(won.description)")
-            Text("yen is \(yen.description)")
-            
-            Button {
-                if won == 1000 {
-                    won = 200
-                } else {
-                    won = 1000
-                }
-            } label: {
-                if won == 1000 {
-                    Text("won become to 200")
-                } else {
-                    Text("won become to 1000")
-                }
-            }
+            Text(returnName())
+            Text(returnName2())
         }
     }
+    
+    func returnName2() -> String {
+        return "sasha"
+    } //method
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

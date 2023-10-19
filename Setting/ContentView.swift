648e3e9074dae5c4470df53bd22e68087067ccf4
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-19 17:30:37 +0900

    class と struct の理解

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -7,24 +7,56 @@
 
 import SwiftUI
 
+struct PersonStruct {
+    var name: String
+    let age: Int
+}
+
+class PersonClass {
+    var name: String
+    let age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
 struct ContentView: View {
     
-    var name: String? = "sasha"
-    var nickName: String?
-    var mail: String? = "abc@def.com"
     
-    var body: some View {
-        VStack{
-            Text(name!)
-            
-            Text(nickName ?? "james")
-            
-            if let mail = mail {
-                Text(mail)
-            } else {
-                Text("Error")
-            }
-        }
+    var sashaCla = PersonClass(name: "sasha", age: 2)
+    @State var nickName: String = "None"
+    @State var nickName2: String = "None"
+    
+   var body: some View {
+       VStack {
+           
+           Text(nickName)
+           Text(nickName2)
+           
+           Button {
+               var sashaStr = PersonStruct(name: "sasha", age: 2)
+               var fakeSashaStr = sashaStr
+               sashaStr.name = "sasha2"
+               fakeSashaStr.name = "sasha3"
+               nickName = sashaStr.name
+               // sasha2 が表示される
+           }label: {
+                Text("Hit")
+           }
+           
+           Button {
+               var sashaCla = PersonClass(name: "sasha", age: 2)
+               var fakeSashaCla = sashaCla
+               sashaCla.name = "sasha2"
+               fakeSashaCla.name = "sasha3"
+               nickName2 = sashaCla.name
+               // sasha3 が表示される
+           }label: {
+                Text("Hit")
+           }
+       }
     }
 }
 

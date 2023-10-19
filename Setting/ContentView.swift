6cc9dcdee507f44deebab4cbc9740eb113f0c421
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-19 18:28:21 +0900

    optional chaining の理解

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -7,54 +7,25 @@
 
 import SwiftUI
 
-struct PersonStruct {
-    var name: String
-    let age: Int
+struct Student {
+    let name: String
+    let pet: Pet?
 }
 
-class PersonClass {
-    var name: String
+struct Pet {
+    let name: String
     let age: Int
-    
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
 }
 
 struct ContentView: View {
-    
-    
-    var sashaCla = PersonClass(name: "sasha", age: 2)
-    @State var nickName: String = "None"
-    @State var nickName2: String = "None"
+    let student = Student(name: "sasha", pet: Pet(name: "puppy", age: 2))
     
    var body: some View {
-       VStack {
-           
-           Text(nickName)
-           Text(nickName2)
-           
-           Button {
-               var sashaStr = PersonStruct(name: "sasha", age: 2)
-               var fakeSashaStr = sashaStr
-               sashaStr.name = "sasha2"
-               fakeSashaStr.name = "sasha3"
-               nickName = sashaStr.name
-               // sasha2 が表示される
-           }label: {
-                Text("Hit")
-           }
-           
-           Button {
-               var sashaCla = PersonClass(name: "sasha", age: 2)
-               var fakeSashaCla = sashaCla
-               sashaCla.name = "sasha2"
-               fakeSashaCla.name = "sasha3"
-               nickName2 = sashaCla.name
-               // sasha3 が表示される
-           }label: {
-                Text("Hit")
+       VStack{
+           Text(student.name)
+//           Text(student.pet!.name)
+           if let petName = student.pet?.name {
+               Text(petName)
            }
        }
     }

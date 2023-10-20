793f403809e541c7167b4b3dcb4ff34d0e37d2e7
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-20 16:45:55 +0900

    securefield の理解

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -8,15 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var userName: String = ""
+    @State var id: String = ""
+    @State var password: String = ""
+    @State var checking: Bool = false
+    @State var success: Bool = false
+    @State var fail: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("userID : \(userName)")
-            TextField("Enter your ID", text: $userName)
+        VStack {
+            TextField("Enter ID", text: $id)
                 .textFieldStyle(.roundedBorder)
+            HStack{
+                if checking {
+                    TextField("Enter Password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                } else {
+                    SecureField("Enter Password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                }
+                Button {
+                    checking.toggle()
+                }label: {
+                    Image(systemName: "eye")
+                }
+            }
+            Button {
+                if password == "1234" {
+                    success = true
+                } else {
+                    fail = true
+                }
+            } label: {
+                Text("Log in")
+            }
+            .fullScreenCover(isPresented: $success) {
+                ZStack{
+                    Color.orange.ignoresSafeArea()
+                    VStack{
+                        Text("Welcome, \(id)")
+                        Button {
+                            success = false
+                            fail = true
+                        }label: {
+                            Text("Log out")
+                        }
+                    }
+                }
+            }
+            .alert(isPresented: $fail) {
+                Alert(title: Text("Password is wrong!"))
+            }
+            .padding()
         }
-        .padding()
     }
 }
 

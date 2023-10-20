62dce7eed11585c65e76c08cc4f7f8f8ce37e08e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-20 14:28:48 +0900

    divider の理解

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -10,8 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
+            Image(systemName: "heart.fill")
+            Divider()
+                .background(.red)
+                .frame(height: 30)
             Text("World")
-                .background(Color("SashaColor"))
+            HStack{
+                Image(systemName: "heart.fill")
+                Divider()
+                    .background(.red)
+                    .frame(width: 30)
+                Text("World")
+            }
+            Image(systemName: "heart.fill")
+            Divider()
+                .background(.red)
+                .frame(height: 30)
+            Text("World")
         }
     }
 }

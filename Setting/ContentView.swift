173de27f912f5f3c10401bcdf661c237179b8263
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-20 14:23:11 +0900

    ダクモード用の color set を追加

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -7,40 +7,11 @@
 
 import SwiftUI
 
-struct Friend {
-    let name: String
-    let pet: Pet?
-}
-
-enum Pet {
-    case cat(String)
-    case dog(String)
-}
-
 struct ContentView: View {
-    var sasha: Friend = Friend(name: "sasha", pet: Pet.cat("corgy"))
-    var nico: Friend = Friend(name: "nico", pet: Pet.dog("shiba"))
-    @State var pet: String = "Do you have a pet?"
-    
     var body: some View {
         VStack{
-            Text(pet)
-            Button {
-                if let petName = nico.pet {
-                    pet = getPetName(petName)
-                } else {
-                    pet = "No, I don't have a pet."
-                }
-            } label: {
-                Text("Push")
-            }
-        }
-    }
-    
-    func getPetName(_ petName: Pet) -> String {
-        switch petName {
-        case .dog(let name): return name
-        case .cat(let name): return name
+            Text("World")
+                .background(Color("SashaColor"))
         }
     }
 }

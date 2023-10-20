c6043fb039386731e12af5092d10ddeabb9cbab4
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-20 15:55:42 +0900

    toggle の理解

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inHome: Bool = true
     
     var body: some View {
-        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1");/*@END_MENU_TOKEN@*/ Image(systemName: "house.fill") }.tag(1)
-            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/; Image(systemName: "bolt.fill") }.tag(2)
-            Text("Tab Content 3").tabItem { Text("Tab Label 3"); Image(systemName: "heart.fill") }.tag(3)
+        VStack{
+            Toggle(isOn: $inHome) {
+                if inHome {
+                    Text("I'm home.")
+                } else {
+                    Text("I'm out.")
+                }
+            }
+            .padding()
+            .toggleStyle(.switch)
+            .tint(.black)
         }
     }
 }

c21a35783b66b7353aa6614911263b7943f3da14
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-24 10:48:10 +0900

    label の理解

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            HStack{
+                Image(systemName: "heart.fill")
+                Text("Label")
+            }
             Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: "heart.fill")
+            
         }
-        .padding()
     }
 }
 

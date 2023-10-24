b67ce9a3159c61e862b8c019e892e070119c2b88
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-24 13:44:38 +0900

    簡単な ui

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -15,38 +15,79 @@ struct ContentView: View {
     @State var isOn: Bool = false
     
     var body: some View {
-        VStack{
-            HStack{
-                Image(systemName: "person.fill")
-                TextField("Enter ID", text: $userID).textFieldStyle(.roundedBorder)
-            }
-            HStack{
-                Image(systemName: "lock.fill")
-                if isOn {
-                    TextField("Enter PW", text: $userPW).textFieldStyle(.roundedBorder)
-                } else {
-                    SecureField("Enter PW", text: $userPW).textFieldStyle(.roundedBorder)
+        NavigationStack {
+            List {
+                Section {
+                    HStack{
+                        Image(systemName: "circle.fill")
+                        VStack(alignment: .leading) {
+                            Text("sasha")
+                            Text("Apple ID, iColud, 미디어 및 구입")
+                        }
+                    }
+
                 }
-                Button {
-                    isOn.toggle()
-                } label: {
-                    Image(systemName: "eye").foregroundColor(.black)
+                
+                Section {
+                    HStack{
+                        Image(systemName: "hourglass")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.indigo)
+                            .foregroundColor(.white)
+                            .cornerRadius(6)
+                        NavigationLink("스크린 타임") {
+                            Text("detail")
+                        }
+                    }
+                }
+                
+                Section {
+                    HStack{
+                        Image(systemName: "gear")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(6)
+                        NavigationLink("일반") {
+                            Text("detail")
+                        }
+                    }
+                    HStack{
+                        Image(systemName: "accessibility")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(6)
+                        NavigationLink("손쉬운 사용") {
+                            Text("detail")
+                        }
+                    }
+                    HStack{
+                        Image(systemName: "heart")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(6)
+                        NavigationLink("개인 정보 보호") {
+                            Text("detail")
+                        }
+                    }
                 }
             }
-            Button {
-                if userID == "1234" && userPW == "1234" {
-                    hasLoggedIn = true
-                } else {
-                    hasLoggedIn = false
-                }
-            } label: {
-                Text("Log In")
-            }
+            .navigationTitle(Text("설정"))
         }
-        .padding()
-        .sheet(isPresented: $hasLoggedIn, content: {
-            Text("Welcome, \(userID)")
-        })
     }
 }
 

c8972e129ebe48c069f3427d5505bab95ec9162e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-24 13:22:52 +0900

    level 2 のまとめ

--- Setting/ContentView.swift
+++ Setting/ContentView.swift
@@ -9,26 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var isViewing: Bool = false
+    @State var userID: String = ""
+    @State var userPW: String = ""
+    @State var hasLoggedIn: Bool = false
+    @State var isOn: Bool = false
     
     var body: some View {
         VStack{
-            Text("Sasha")
-                .sheet(isPresented: $isViewing) {
-                Text("Modal")
-                        .onAppear {
-                            print("on Appear")
-                        }
-                        .onDisappear {
-                            print("on Disaapear")
-                        }
-                }
-            Button {
-                isViewing.toggle()
-            } label: {
-                Text("Click")
+            HStack{
+                Image(systemName: "person.fill")
+                TextField("Enter ID", text: $userID).textFieldStyle(.roundedBorder)
             }
-        }  
+            HStack{
+                Image(systemName: "lock.fill")
+                if isOn {
+                    TextField("Enter PW", text: $userPW).textFieldStyle(.roundedBorder)
+                } else {
+                    SecureField("Enter PW", text: $userPW).textFieldStyle(.roundedBorder)
+                }
+                Button {
+                    isOn.toggle()
+                } label: {
+                    Image(systemName: "eye").foregroundColor(.black)
+                }
+            }
+            Button {
+                if userID == "1234" && userPW == "1234" {
+                    hasLoggedIn = true
+                } else {
+                    hasLoggedIn = false
+                }
+            } label: {
+                Text("Log In")
+            }
+        }
+        .padding()
+        .sheet(isPresented: $hasLoggedIn, content: {
+            Text("Welcome, \(userID)")
+        })
     }
 }
 
